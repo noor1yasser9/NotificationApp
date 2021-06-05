2def188a5a5735b69a32b32bda9a4caf380ba05a
OMEN <mrh61037@gmail.com>
2021-06-05 20:10:04 +0300

    add file others ResultRequest.kt and string csontant

---
+++ app/src/main/java/com/nurbk/ps/notificationapp/other/StringConstant.kt
@@ -0,0 +1,11 @@
+package com.nurbk.ps.notificationapp.other
+
+
+
+
+const val VALUE_TYPE = "application/json"
+const val AUTH_VALUE ="AAAAv_TWHvM:APA91bG5heOmX2U23WwhUTijLWUYN7Efrscg8jOpG2KKsko6Eg0SwiGA34h31RCB7jtGHCgLt3IxACcy__SXc0We96gkleTxGsfM9ihSg4mUhI1843tC2FOOEVxmwCCvdGN93FR7MoW1"
+
+const val BASE_URL = "https://fcm.googleapis.com/fcm/"
+
+
